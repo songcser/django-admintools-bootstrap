5444a1e4803003a03cd9329fbd33ac64c60060da
songjy <songjy@ethical.cn>
2016-06-14 14:26:36 +0800

    BeautifulSoup update to 4.4.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 install_requires = [
     'django-admin-tools>=0.4.1',
-    'BeautifulSoup>=3.2.0',
+    'BeautifulSoup4>=4.4.0',
     'django-appconf>=0.4.1',
     'versiontools>=1.8.2',
     'django-mediagenerator==1.11',
